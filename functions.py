5b6d8721f485ddc3aef7699333830bd15b60c4f6
TaniksB <krmalkama@gmail.com>
2026-02-27 10:45:01 +0100

    add command recognition, functions.py

---
+++ functions.py
@@ -0,0 +1,23 @@
+def display_report():
+    print ("to be implemented")
+    return None
+
+def wipe_list():
+    print ("to be implemented")
+    return None
+
+def display_list():
+    print ("to be implemented")
+    return None
+
+def wipe_log():
+    print ("to be implemented")
+    return None
+
+def log_add():
+    print ("to be implemented")
+    return None
+
+def log_cons(inp):
+    print ("to be implemented")
+    return None
---
+++ main.py
@@ -0,0 +1,33 @@
+from functions import *
+import sys
+
+cmds = (".list", ".wipelist", ".report", ".wipe", ".new", ".q")
+
+print ("Usage:")
+print (".list - displays list of saved food items")
+print (".wipelist - deletes list of saved food items")
+print (".report - displays weekly calorie and nutrient report")
+print (".wipe - deletes all logged consumption")
+print (".new - begins process to save a new food item to list")
+print (".q - ends the program")
+print ("Any other input will attempt to log consumption")
+
+while 1 < 2:
+    inp = input()
+    if inp == ".report":
+        display_report()
+    if inp == ".list":
+        display_list()
+    if inp == ".wipelist":
+        wipe_list()
+    if inp == ".wipe":
+        wipe_log()
+    if inp == ".new":
+        log_add()
+    if inp == ".q":
+        sys.exit()
+    if inp not in cmds:
+        log_cons(inp)
+    inp = None
+
+
